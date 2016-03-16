e133ec5df3ba9499edd0d45aae7270e207ce5745
Dave Mueller <dave@perljedi.com>
2016-03-16 09:18:26 -0400

    refactor to avoid redundant or layered spy objects

--- lib/Jasmine/Spy.pm
+++ lib/Jasmine/Spy.pm
@@ -5,13 +5,13 @@ package Jasmine::Spy;
 use vars qw(@EXPORT @EXPORT_OK %EXPORT_TAGS);
 use base qw(Exporter);
 use Class::MOP;
-my(%spies) = ();
+my (%spies) = ();
 
 BEGIN {
     @EXPORT    = ();
     @EXPORT_OK = qw(
         spyOn
-		stopSpying
+        stopSpying
     );
     %EXPORT_TAGS = (
 
@@ -20,16 +20,21 @@ BEGIN {
 
 sub spyOn {
     my ($proto, $method) = @_;
-    my $spy = Jasmine::Spy::Instance->new($proto, $method);
-	$spies{$proto} = $spy;
-	return $spy;
+    if(exists($spies{$proto})){
+        $spies{$proto}->setSpyMethod($proto, $method);
+    }
+    else {
+        my $spy = Jasmine::Spy::Instance->new($proto, $method);
+        $spies{$proto} = $spy;
+    }
+    return $spies{$proto};
 }
 
 sub stopSpying {
-	my($proto) = @_;
-	while(exists $spies{$proto}){
-		$spies{$proto}{class}->meta->rebless_instance_back($proto);
-	}
+    my ($proto) = @_;
+    while (exists $spies{$proto}) {
+        $spies{$proto}{class}->meta->rebless_instance_back($proto);
+    }
 }
 
 package Jasmine::Spy::Instance;
@@ -39,40 +44,48 @@ sub new {
     my $class = ref($proto) || $proto;
     eval "package $class; use metaclass;" unless ($proto->can("metaclass"));
 
-    my $metaclass = $proto->meta;
-    $metaclass->make_mutable if ($metaclass->is_immutable);
 
-    my $self = {
-        proto  => $proto,
-        method => $method,
-		class  => $class,
-    };
-
+    my $self = bless(
+        {
+            proto => $proto,
+            class => $class,
+        },
+        ref($mp) || $mp
+    );
     if (ref($proto)) {
-        my $spyClass = Class::MOP::Class->create_anon_class(
-            methods => {
-                $method => sub { return undef },
-            },
-            superclasses => [$class],
-        );
+        my $spyClass = Class::MOP::Class->create_anon_class(superclasses => [$class]);
         $spyClass->rebless_instance($proto);
         $self->{spyClass} = $spyClass;
     }
-    else {
-        $self->{original_methods}{$method} = $metaclass->remove_method($method);
-		$self->{spyClass} = $metaclass;
-        $metaclass->add_method($method, sub { return undef; });
-    }
-    return bless($self, ref($mp) || $mp);
+
+    $self->setSpyMethod($proto, $method);
+
+    return $self;
+}
+
+sub setSpyMethod {
+    my($self, $proto, $method) = @_;
+
+    my $class = ref($proto) || $proto;
+    my $metaclass = $proto->meta;
+    $metaclass->make_mutable if ($metaclass->is_immutable);
+
+    $self->{current_method} = $method;
+    $self->{original_methods}{$method} = $metaclass->remove_method($method);
+    $self->{spyClass} = $metaclass;
+    $metaclass->add_method($method, sub { return undef; });
 }
 
 sub andReturn {
-	my $self = shift;
-	my $ret = shift;
-	$self->{spyClass}->remove_method($self->{method});
-	$self->{spyClass}->add_method($self->{method}, sub {
-		return $ret;
-	});
+    my $self = shift;
+    my $ret  = shift;
+    $self->{spyClass}->remove_method($self->{current_method});
+    $self->{spyClass}->add_method(
+        $self->{current_method},
+        sub {
+            return $ret;
+        }
+    );
 }
 
 return 42;
